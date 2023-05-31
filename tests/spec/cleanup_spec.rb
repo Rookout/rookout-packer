99c3bc0c343665a40acec411a87f573bf9f5224e
Alexey Gutkin <alexey@rookout.com>
2023-05-31 19:10:12 +0300

    cleanup

---
+++ tests/spec/cleanup_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe service('rookout-controller') do
+    it { should be_enabled }
+    it { should_not be_running }
+end
+
+describe command("cat /etc/rookout/config | grep ROOKOUT_TOKEN=dummy_token") do
+    its(:stdout) { should eq 'ROOKOUT_TOKEN=dummy_token' }
+end
