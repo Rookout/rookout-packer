e1a38de6d4af98f09f289fbb125aaaa83b203603
Alexey Gutkin <alexey@rookout.com>
2023-05-31 19:28:56 +0300

    cleanup

--- scripts/cleanup.sh
+++ scripts/cleanup.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-sudo sed -i 's/${ROOKOUT_TOKEN}/dummy_token/g' /etc/rookout/config
+sudo sed -i "s/${ROOKOUT_TOKEN}/dummy_token/g" /etc/rookout/config
 
 sudo systemctl restart rookout-controller.service
 sudo systemctl restart rookout-data-on-prem.service
