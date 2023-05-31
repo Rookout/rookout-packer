1714104826bda925275220d0e8872552a423d735
Alexey Gutkin <alexey@rookout.com>
2023-05-31 19:18:38 +0300

    cleanup

--- scripts/install-docker-ubuntu.sh
+++ scripts/install-docker-ubuntu.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
-set -e
-
 sudo apt-get update
 sudo apt-get install \
     ca-certificates \
--- tests/spec/cleanup_spec.rb
+++ tests/spec/cleanup_spec.rb
@@ -1,10 +1,12 @@
 require 'spec_helper'
 
-describe service('rookout-controller') do
-    it { should be_enabled }
-    it { should_not be_running }
-end
+describe 'Validate token is removed' do
+    describe service('rookout-controller') do
+        it { should be_enabled }
+        it { should_not be_running }
+    end
 
-describe command("cat /etc/rookout/config | grep ROOKOUT_TOKEN=dummy_token") do
-    its(:stdout) { should eq 'ROOKOUT_TOKEN=dummy_token' }
+    describe command("cat /etc/rookout/config | grep ROOKOUT_TOKEN=dummy_token") do
+        its(:stdout) { should eq 'ROOKOUT_TOKEN=dummy_token' }
+    end
 end
